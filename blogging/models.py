8ffca5f0d6e5628228f51795ffeae37555035e71
kylenod <62862441+kylenod@users.noreply.github.com>
2021-09-29 19:47:33 -0400

    Updated Category to show in Post Admin

--- blogging/models.py
+++ blogging/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
 
+
     def __str__(self):
         return self.title
 
@@ -18,10 +19,13 @@ class Post(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    posts = models.ForeignKey(Post, blank=True, related_name='categories', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+
+
