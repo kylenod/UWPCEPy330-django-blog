cd86e8e3d866d313e7dbbe65c5798a0fbe655ba4
kylenod <62862441+kylenod@users.noreply.github.com>
2021-09-29 22:20:43 -0400

    Removed unnecessary PostInLine class

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 
+from django.contrib import admin
+
 # Register your models here.
-from blogging.models import Post
-admin.site.register(Post)
+from blogging.models import Post, Category
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+# class PostInline(admin.TabularInline):
+#     model = Post
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ['posts']
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
